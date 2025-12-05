a5e72425f2e64e2571119d270d21c2a971cd4e7e
Marcelo Magalhães <128628299+marcelomagalhaes1@users.noreply.github.com>
2025-12-05 19:41:19 -0300

    Update auth_provider.dart

--- rick_morty_wiki_1/lib/providers/auth_provider.dart
+++ rick_morty_wiki_1/lib/providers/auth_provider.dart
@@ -3,48 +3,44 @@ import 'package:hive/hive.dart';
 import '../models/usuario_model.dart';
 
 class AuthProvider extends ChangeNotifier {
-  Usuario? _usuarioLogado; // Guarda quem está usando o app agora
+  Usuario? _usuarioLogado;
 
   Usuario? get usuarioLogado => _usuarioLogado;
   bool get estaLogado => _usuarioLogado != null;
 
-  // Função para Cadastrar
   Future<bool> cadastrar(String email, String senha) async {
     var box = Hive.box<Usuario>('box_usuarios');
 
-    // Verifica se já existe esse email (Varrendo a lista toda)
     for (var u in box.values) {
       if (u.email == email) {
-        return false; // Já existe, não cadastra
+        return false;
       }
     }
 
-    // Se não existe, cria e salva
     var novoUsuario = Usuario(email: email, senha: senha);
     await box.add(novoUsuario);
-    return true; // Sucesso
+    return true;
   }
 
-  // Função para Logar
   Future<bool> logar(String email, String senha) async {
     var box = Hive.box<Usuario>('box_usuarios');
 
     try {
-      // Procura o primeiro usuário que bater email e senha
       var usuarioEncontrado = box.values.firstWhere(
               (u) => u.email == email && u.senha == senha
       );
 
       _usuarioLogado = usuarioEncontrado;
-      notifyListeners(); // AVISA AS TELAS QUE MUDOU O ESTADO!
+      notifyListeners();
       return true;
     } catch (e) {
-      return false; // Não achou ninguém
+      return false;
     }
   }
 
   void deslogar() {
     _usuarioLogado = null;
-    notifyListeners(); // Avisa a tela para voltar pro Login
+    notifyListeners();
   }
+
 }
